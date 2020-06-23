61470486e27772d6dfbb150ba0dfec7e501f5489
rosscodingrockstars <hey@jenniferross.tech>
2020-06-23 14:01:03 -0400

    started js

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-//const firstCol = document.getElementById("first-col");
-//const food = "lemon-bread";
-//firstCol.innerHTML = `<p>I like labradoodles and ${food}. </p>`;
-const numArray = [1, 2, 3, 4, 5];
-let result = 0;
-for(let i = 0; i < numArray.length; i++) {
-let currentNumber = numArray[i]
-result += currentNumber;
-console.log(result);
-}
+$("#wins").text("1")
+
+$("#icecream").click(function() {
+
+    alert("Amount of coins collected")
+})
+
+$(".btn").click(function() {
+
+    alert(";pLaying")
+})
