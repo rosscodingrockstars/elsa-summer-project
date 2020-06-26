c7c4bcfecb322b3da8f2f8543a7b78860a95398c
rosscodingrockstars <hey@jenniferross.tech>
2020-06-26 14:03:54 -0400

    generating random numbers

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
-$("#wins").text("1")
+$("#wins").text("1");
 
 $("#icecream").click(function() {
-
-    alert("Amount of coins collected")
-})
+  alert("Amount of coins collected");
+});
 
 $(".btn").click(function() {
+  alert(";pLaying");
+});
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  let targetNumber = Math.floor(Math.random() * (max - min)) + min;
+  $("#target-number").text(targetNumber);
 
-    alert(";pLaying")
-})
+}
+
+getRandomInt(19, 120);
