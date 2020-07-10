28b5645efed3778903c40a0d9dd210a49ea3a3b1
rosscodingrockstars <hey@jenniferross.tech>
2020-07-10 14:02:30 -0400

    added the wins and losses

--- main.js
+++ main.js
@@ -11,36 +11,45 @@ let targetNumber = Math.floor(Math.random() * (120 - 19)) + 19;
 $("#target-number").text(targetNumber);
 let points = 0;
 $("#points").text(points);
-if(targetNumber < points) {
-  console.log("you lost")
-} else {
-  console.log("maybe you won")
+function checkPoints() {
+  if (targetNumber < points) {
+    losses++;
+  } else if (points === targetNumber) {
+    wins++;
+  } else {
+    console.log("keep going");
+  }
 }
-let treatOne = Math.floor(Math.random() * 12 + 1);
-let treatTwo = Math.floor(Math.random() * 12 + 1);
-let treatThree = Math.floor(Math.random() * 12 + 1);
-let treatFour = Math.floor(Math.random() * 12 + 1);
-let treatFive = Math.floor(Math.random() * 12 + 1);
+let treatOne = Math.floor(Math.random() * 12 + 2);
+let treatTwo = Math.floor(Math.random() * 12 + 2);
+let treatThree = Math.floor(Math.random() * 12 + 2);
+let treatFour = Math.floor(Math.random() * 12 + 2);
+let treatFive = Math.floor(Math.random() * 12 + 2);
 let wins = 0;
 let losses = 0;
 
 $("#cupcake").click(function() {
   points += treatOne;
   $("#points").text(points);
+  checkPoints();
 });
 $("#donut").click(function() {
   points += treatTwo;
   $("#points").text(points);
+  checkPoints();
 });
 $("#icecream").click(function() {
   points += treatThree;
   $("#points").text(points);
+  checkPoints();
 });
 $("#cookie").click(function() {
   points += treatFour;
   $("#points").text(points);
+  checkPoints();
 });
 $("#cake").click(function() {
   points += treatFive;
   $("#points").text(points);
+  checkPoints();
 });
